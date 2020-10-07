8359c4c00ecd8a2fb66b3c7f90053f8551985b10
ashu8912 <ashughildiyal5@gmail.com>
2020-10-07 17:23:17 +0530

    init app

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+require('update-electron-app')()
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
