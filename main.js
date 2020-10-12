1ac7fc16cb4c6c311b6090cc77683dbdc9f506c4
ashu8912 <ashughildiyal5@gmail.com>
2020-10-12 13:15:35 +0530

    new release done

--- main.js
+++ main.js
@@ -12,29 +12,13 @@ function sendStatusToWindow(text) {
   win.webContents.send('message', text);
 }
 
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
   sendStatusToWindow(log_message);
 })
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
 
-autoUpdater.autoDownload = true
 function createWindow () {
   // Create the browser window.
    win = new BrowserWindow({
@@ -47,7 +31,21 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
-  autoUpdater.checkForUpdatesAndNotify()
+  win.once('ready-to-show', () => {
+      autoUpdater.checkForUpdatesAndNotify();
+  })
 }
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+}); 
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
 
 app.whenReady().then(createWindow)
