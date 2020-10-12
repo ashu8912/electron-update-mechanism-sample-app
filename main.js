cc0ffd73d099386f8f0c552b20b0013880228ede
ashu8912 <ashughildiyal5@gmail.com>
2020-10-12 14:54:35 +0530

    new changes

--- main.js
+++ main.js
@@ -1,48 +1,44 @@
-const { app, BrowserWindow,ipcMain } = require('electron')
-const log = require('electron-log');
-const {autoUpdater} = require("electron-updater");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
-let win;
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
-
-function sendStatusToWindow(text) {
-  log.info(text);
-  win.webContents.send('message', text);
-}
-
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-
-autoUpdater.autoDownload = true
+let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-   win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  // and load the index.html of the app.
-  win.loadFile('index.html')
-  autoUpdater.checkForUpdatesAndNotify();
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-}); 
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
 
 autoUpdater.on('update-available', () => {
   mainWindow.webContents.send('update_available');
@@ -52,4 +48,6 @@ autoUpdater.on('update-downloaded', () => {
   mainWindow.webContents.send('update_downloaded');
 });
 
-app.whenReady().then(createWindow)
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
